5701b2d0d8d7b079320b1d346a33353367c4ff8d
Padchara <icebub@gmail.com>
2023-06-10 12:49:15 +0100

    added clean data and tokenizer preprocess

--- filmfinder/src/datasets/MovieGenres.py
+++ filmfinder/src/datasets/MovieGenres.py
@@ -2,18 +2,37 @@ from collections import defaultdict
 
 import pandas as pd
 import torch
+import tqdm
 from torch.utils.data import Dataset
 
 
 class MovieGenres(Dataset):
-    def __init__(self, file_path):
+    def __init__(self, file_path, tokenizer, max_len=512):
         metadata = pd.read_csv(file_path, low_memory=False)
-        self.overviews = metadata["overview"].values
+        metadata = metadata.dropna(subset=["overview"])
+
+        overviews = metadata["overview"].values
         genres = metadata["genres"].values
 
         self.genre_count = self.genres_count(genres)
         self.mapping, self.reverse_mapping = self.genres_mapping(self.genre_count)
         self.label = self.create_label(genres, self.mapping)
+
+        self.train = self.preprocess(overviews, tokenizer, max_len)
+
+    def preprocess(self, sentences, tokenizer, max_len=512):
+        print("Preprocessing...")
+        tokenized_sentences = []
+        for sentence in tqdm.tqdm(sentences):
+            tokens = tokenizer.tokenize(sentence)
+            tokens = ["[CLS]"] + tokens + ["[SEP]"]
+            token_ids = tokenizer.convert_tokens_to_ids(tokens)
+            token_ids = token_ids[:max_len]  # limit to max_len
+            padding_length = max_len - len(token_ids)
+            token_ids += [0] * padding_length  # pad
+            tokenized_sentences.append(token_ids)
+
+        return torch.tensor(tokenized_sentences)
 
     def genres_count(self, genres):
         genre_count = defaultdict(int)
@@ -39,10 +58,10 @@ class MovieGenres(Dataset):
             for name in names:
                 now_label[mapping[name]] = 1
             labels.append(now_label)
-        return labels
+        return torch.tensor(labels)
 
     def __getitem__(self, index):
-        x = self.overviews[index]
+        x = self.train[index]
         y = self.label[index]
 
         return x, y
@@ -54,8 +73,11 @@ class MovieGenres(Dataset):
 if __name__ == "__main__":
     import os
 
+    from transformers import BertTokenizer
+
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
     abs_folder = os.path.dirname(os.path.abspath(__file__))
-    dataset = MovieGenres(f"{abs_folder}/../data/movies_metadata.csv")
+    dataset = MovieGenres(f"{abs_folder}/../data/movies_metadata.csv", tokenizer)
     x, y = dataset[0]
     print(x)
     print(y)
--- filmfinder/src/train.py
+++ filmfinder/src/train.py
@@ -1,5 +1,26 @@
+import os
+
+from datasets.MovieGenres import MovieGenres
 from models.BaseModel import BaseModel
+from transformers import BertTokenizer
 
-model = BaseModel(num_classes=2, freeze_bert=False)
+model = BaseModel(num_classes=32, freeze_bert=False)
 
-print(model)
+abs_folder = os.path.dirname(os.path.abspath(__file__))
+dataset = MovieGenres(f"{abs_folder}/data/movies_metadata.csv")
+x, y = dataset[0]
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+sentence = x
+tokens = tokenizer.tokenize(sentence)
+
+tokens = ["[CLS]"] + tokens + ["[SEP]"]
+
+# Convert tokens to token IDs
+token_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+# Print the token IDs
+
+print(tokens)
+print(token_ids)
